d033fae065a13f5522f57dfcce090c45bdbfab10
Neil Cudden <ncudden@sbcglobal.net>
2017-05-10 14:54:41 -0700

    Initial hangman version completed

--- Hangman.rb
+++ Hangman.rb
@@ -20,17 +20,14 @@ module HangmanMethods
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
   def start_game
-
     @finish = false
-
     puts " "
-    puts "Welcome, foul miscreant! Is this a new game or are you"
-    puts "continuing an old one?"
+    puts "Welcome! Is this a new game or"
+    puts "are you continuing an old one?"
     puts " "
     puts "Enter \"1\" to start a new game"
     puts "Enter \"2\" to continue a saved game"
     puts " "
-
     response = false
     while response == false
       answer = gets.chomp.to_s
@@ -52,7 +49,6 @@ module HangmanMethods
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
   def new_game
-
     @guesses = []
     @wrong_guesses = []
     @fail = 7
@@ -61,7 +57,6 @@ module HangmanMethods
     words.each do |w|
       w.gsub!("\\","")
     end
-
     words_sized = []
     words.each do |l|
       if l.length > 4 && l.length <13
@@ -69,54 +64,30 @@ module HangmanMethods
       else next
       end
     end
-
     word = words_sized[rand(words_sized.length)].upcase
     @letters = word.split("")[0..-2]
     #puts "#{@letters}"         # to be removed when game completed
-
     @num_letters = @letters.length
     @underline = "-- "
-
     for num in (0..@num_letters - 1)
       @guesses[num] = " "
     end
-
     game_save
-
   end
 
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
   def game_save
-
     @letters_map = @letters.map {|x| x.to_s}
     @out_letters = @letters_map.join
-
     @out_num_letters = @num_letters.to_s
-
     @guesses_map = @guesses.map {|x| x.to_s}
     @out_guesses = @guesses_map.join
-
     @wrong_guesses_map = @wrong_guesses.map {|x| x.to_s}
     @out_wrong_guesses = @wrong_guesses_map.join
-
     @out_fail = @fail.to_s
-
-#    @gallows_map = @gallows.map {|x| x.to_s}
-#    @out_gallows = @gallows_map.join
     @out_gallows = @gallows
-
     @out_underline = @underline
-
-    puts "My Output Format: \n"
-    puts "#{@out_letters}"
-    puts "#{@out_num_letters}"
-    puts "#{@out_guesses}"
-    puts "#{@out_wrong_guesses}"
-    puts "#{@out_fail}"
-    puts "#{@out_gallows}"
-    puts "#{@out_underline}"
-
     @time = Time.new
     @my_save = File.open("hangman_save#{@time.day}#{@time.hour}#{@time.min}#{@time.sec}.txt", "w") do |f|
       f.puts "#{@out_letters}"
@@ -127,44 +98,31 @@ module HangmanMethods
       f.puts "#{@out_gallows}"
       f.puts "#{@out_underline}"
     end
-
   end
 
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
   def continue_game
-
     puts "Enter saved game you wish to continue:"
     @renew = gets.chomp
     @saved_output = File.open(@renew)
-    #@saved_output = File.open("hangman_save9112820.txt")
-
     @in_letters = @saved_output.gets
     @letters = @in_letters.split("")
     @letters.delete("\n")
-    #puts "#{@letters}"
-
     @in_num_letters = @saved_output.gets
     @num_letters = @in_num_letters.to_i
-    #puts @num_letters.is_a? Integer
-
     @in_guesses = @saved_output.gets
     @guesses = @in_guesses.split("")
     @guesses.delete("\n")
-    #puts "#{@guesses}"
-
     @in_wrong_guesses = @saved_output.gets
     @wrong_guesses = @in_wrong_guesses.split("")
     @wrong_guesses.delete("\n")
-    #puts "#{@wrong_guesses}"
-
     @in_fail = @saved_output.gets
     @fail = @in_fail.to_i
-    #puts @fail.is_a? Integer
-
     @in_gallows = @saved_output.gets
     @in_gallows.gsub!("[", " ")
     @in_gallows.gsub!("]", "")
+    @in_gallows.gsub!("|\\\\", "|\\")
     @gallows = @in_gallows.split(",")
     @gallows.each do |w|
       w.gsub!("\" ","")
@@ -175,15 +133,11 @@ module HangmanMethods
     @gallows.each do |w|
       w.gsub!("\n","")
     end
-    #puts @gallows
-    #puts "#{@gallows}"
-
   end
 
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
   def game_setup
-
     @level0 = "    ____    "
     @level1 = "   |    |   "
     @level2 = "   |        "
@@ -202,21 +156,16 @@ module HangmanMethods
     @wrong52 = "   |   / \\  "
 
     @gallows = [@level0, @level1, @level2, @level3, @level4, @level5, @level6, @level7]
-
   end
 
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
   def plot
-
-    #greeting
     puts @gallows
     puts " "
-
     for tmplength in 0..@guesses.length
       print "#{@guesses[tmplength]}  "
     end
-
     puts " "
     @num_letters.times {print @underline}
     puts " "
@@ -229,25 +178,21 @@ module HangmanMethods
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
   def turns
-
     while @finish == false
       plot
       take_a_guess
       game_over
-      quit
+      continue
     end
-
     if @finish == true
       goodbye
     else
     end
-
   end
 
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
   def take_a_guess
-
     puts " "
     puts "Enter your letter guess:"
     valid_answer = false
@@ -256,12 +201,11 @@ module HangmanMethods
         if @howbout == (0..9)
           puts "No, not a number...enter a letter, a letter, stupid!"
         elsif @howbout.length > 1
-          puts "Uhh, enter just ONE letter, ya moron!"
+          puts "Uhh, enter just ONE letter, mmmmm...O-kay?"
         else 
           valid_answer = true
         end
     end
-
     @hitcount = 0
     for index in 0...@num_letters
       if @letters[index] == @howbout
@@ -300,42 +244,51 @@ module HangmanMethods
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
   def game_over
-
     if @guesses == @letters
-      puts " "
-      puts "Game Over....We have a winner!!"
-      puts " "
       plot
+      puts " "
+      puts "************************"
+      puts "*                      *"
+      puts "*  We have a winner!!  *"
+      puts "*                      *"
+      puts "************************"
+      puts " "
       @finish = true
     elsif @fail == 0
-      puts " "
-      puts "Game Over....Dead man hanging!"
-      puts " "
       plot
+      puts " "
+      puts "************************"
+      puts "*                      *"
+      puts "*  Dead man hanging!!  *"
+      puts "*                      *"
+      puts "************************"
+      puts " "
       @finish = true
     else
     end
-
   end
 
 # >< <> >< <> >< <> >< >< <> >< <> >< <> >< >< <> >< <>
 
-  def quit
-    puts " "
-    puts "Continue game or save & quit? (enter C or S)"
-    answer = false
-    while answer == false
-      cont = gets.chomp.upcase.to_s
-      if (cont != "C" && cont != "S")
-        puts "Enter a \"C\" or an \"S\" please!"
-        answer = false
-      elsif cont == "S"
-        game_save
-        puts "Saving game before exiting...done"
-        answer = true
-        @finish = true
-      else
-        answer = true
+  def continue
+    unless (@fail == 0) || (@guesses == @letters)
+      puts " "
+      puts "Continue game or save & quit? (enter C or Q)"
+      answer = false
+      while answer == false
+        cont = gets.chomp.upcase.to_s
+        if (cont != "C" && cont != "Q")
+          puts "Enter a \"C\" or an \"Q\" please!"
+          answer = false
+        elsif cont == "Q"
+          game_save
+          puts "Saving game before exiting...done"
+          answer = true
+          @finish = true
+        elsif cont == "C"
+          answer = true
+        else 
+        end
       end
     end
   end
@@ -344,7 +297,13 @@ module HangmanMethods
 
   def goodbye
     puts " "
-    puts "GAME OVER...the word was \"#{@letters.join("")}\""
+    if (@fail != 0) && (@guesses != @letters)
+      puts "GAME OVER...saved in \"hangman_save#{@time.day}#{@time.hour}#{@time.min}#{@time.sec}.txt\""
+    elsif @fail == 0
+      puts "***   GAME OVER...the word was \"#{@letters.join("")}\"   ***"
+    else
+      puts "***   GAME OVER   ***"
+    end
     puts " "
   end
 
